9061e2c960410ea36fc7635a139c9fa1d4b228ad
luisdomene4 <139975871+luisdomene4@users.noreply.github.com>
2024-10-25 17:44:19 +0200

    hola buenas tardes

---
+++ Autoencoder.py
@@ -0,0 +1,41 @@
+import torch 
+from torch.functional import F 
+from torch import nn
+
+
+class Autoencoder(torch.nn.Module):
+
+    
+    def __init__(self):
+
+        super().__init__()
+        self.encoder = torch.nn.Sequential(
+            # Input = (Batch_size, C, W, H) 
+            # (Batch_size, C, W, H) -> (Batch_size, 16, W/2, H/2) 
+            nn.Conv2d(3, 16, kernel_size = 3, stride=2, padding=1),
+            nn.ReLU(),
+            
+            # (Batch_size, 16, W/2, H/2) -> (Batch_size, 32, W/4, H/4)
+            nn.Conv2d(16, 32, kernel_size = 3, stride=2, padding=1),
+            nn.ReLU(),
+
+            # (Batch_size, 32, W/2, H/2) -> (Batch_size, 64, W/8, H/8)
+            nn.Conv2d(32, 64, kernel_size = 3, stride=2, padding=1),
+            nn.ReLU(),
+
+        )
+        self.bottleneck = torch.nn.Sequential(
+
+          
+        ) 
+
+        self.decoder = torch.nn.Sequential(
+          
+        )
+    
+    
+    def forward(self, x) -> None:
+
+        self.encoded  = self.encoder(x)
+        self.bottleneck = self.bottleneck(self.encoded)
+        self.decoded = self.decoder(self.bottleneck)
