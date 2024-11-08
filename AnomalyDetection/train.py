e25431dbe57ee88aefb97d691c7d825e03e6dc05
luisdomene4 <139975871+luisdomene4@users.noreply.github.com>
2024-11-08 14:16:39 +0100

    train includes wandb, new autoencoder_big part, Autoencoder now includes three channels

--- AnomalyDetection/train.py
+++ AnomalyDetection/train.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import wandb
 from Autoencoder import Autoencoder 
 from utils import HelicoDatasetAnomalyDetection
 from torch.utils.data import DataLoader
@@ -18,7 +19,7 @@ def train(model, loss_function, optimizer, dataset, device, num_epochs=10):
 	:param num_epochs: The number of epochs to train for
 	"""
 	model = model.to(device) 
-	dataloader = DataLoader(dataset, batch_size=128, shuffle=True)
+	dataloader = DataLoader(dataset, batch_size=wandb.config.batch_size, shuffle=True)
 	print("Starting training")
 	for epoch in range(num_epochs):
 		model.train()
@@ -31,18 +32,36 @@ def train(model, loss_function, optimizer, dataset, device, num_epochs=10):
 			loss.backward()
 			optimizer.step()
 			total_loss += loss.item()
-		print(f"Epoch {epoch + 1}, Loss: {total_loss / len(dataset)}")
+		avg_loss = total_loss / len(dataset)
+		print(f"Epoch {epoch + 1}, Loss: {avg_loss}")
+		wandb.log({"epoch": epoch + 1, "loss": avg_loss})
 
 if __name__ == "__main__":
+	# Initialize wandb
+	wandb.init(project="autoencoder-training")
+
+	# Set hyperparameters
+	wandb.config = {
+		"learning_rate": 0.001,
+		"epochs": 10,
+		"batch_size": 128,
+		"optimizer" : "adam"
+	}
+
 	# Load the dataset
 	dataset = HelicoDatasetAnomalyDetection()
 	# Initialize the model
 	model = Autoencoder()
 	loss_function = nn.MSELoss()
-	optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+	# No se si tiene mucho sentido probar con distintos optimizadores que no sean Adam
+	# optimizer = torch.optim.SGD(model.parameters(), lr=wandb.config.learning_rate)
+	# optimizer = torch.optim.Adam(model.parameters(), lr=wandb.config.learning_rate)
+	# optimizer = torch.optim.RMSprop(model.parameters(), lr=wandb.config.learning_rate)
+	optimizer = torch.optim.Adam(model.parameters(), lr=wandb.config.learning_rate)
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 	print(f"Using device {device}")
 	# Train the model
-	train(model, loss_function, optimizer, dataset, device)
+	train(model, loss_function, optimizer, dataset, device, num_epochs=wandb.config.epochs)
 	# Save the model
 	torch.save(model.state_dict(), "model.pth")
+	wandb.save("model.pth")
